36872a5b9e6cec389752cd35a2cab388f019a0be
Sr4B702 <48265543+Sr4B702@users.noreply.github.com>
2019-03-08 02:06:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
+const myid = ['552647908478943243'];
 const prefix = ['-'];
 
 client.on('ready', () => {
