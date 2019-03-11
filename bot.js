4adb68ed20a4327c79a48cb2fa0281051a536bf4
Sr4B702 <48265543+Sr4B702@users.noreply.github.com>
2019-03-12 00:27:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['552647908478943243'];
@@ -47,7 +47,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**- ابجد هوز حطي كلمن سعفس كرشط برشط هرشطط **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -56,7 +56,36 @@ if (message.content === prefix+'spam') {
       }
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+client.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "say")){
+        let args = '';
+        let fltr = m => m.author.id === msg.author.id
+        await msg.reply("**اكتب شي لجعلي اكتب شيئ الان**").then(p =>{
+            p.delete(2222)
+        })
+        msg.channel.awaitMessages(fltr, {
+time: 60000,
+max: 1
+        })
+        .then(sy => {
+            args = sy.first().content
+            sy.first().delete();
+            msg.delete(2222);
+            msg.channel.send(args)
+        })
+    }
+})
 
 client2.on('message', message => {
     if(message.content === prefix+'راتب'){
@@ -81,7 +110,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** - آبجد هوز حطي كلمن صعفز كرشط برشط هرشط **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -89,41 +118,27 @@ if (message.content === prefix+'spam') {
         }
       }
 });
-client2.on('message', message => {
-  if (message.author.bot) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-  if (command === "-say") {
-
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
-
-
-});
-client.on('message', message => {
-  if (message.author.bot) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-  if (command === ".say") {
-
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-  }
-
-
-});
-
+client2.on("message",async msg => {
+    if(msg.content.startsWith(prefix + "say")){
+        if(msg.author.id !== '552647908478943243') return;
+        let args = '';
+        let fltr = m => m.author.id === msg.author.id
+        await msg.reply("**اكتب شي لجعلي اكتب شيئ الان**").then(p =>{
+            p.delete(2222)
+        })
+        msg.channel.awaitMessages(fltr, {
+time: 60000,
+max: 1
+        })
+        .then(sy => {
+            args = sy.first().content
+            sy.first().delete();
+            msg.delete(2222);
+            msg.channel.send(args)
+        })
+    }
+})
 
 
 client.login(process.env.TOKEN);
